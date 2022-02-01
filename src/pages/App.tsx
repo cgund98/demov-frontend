75bdae9d25985d058a7a3381a0daeb6d8f0489f9
Callum Gundlach <gundlachcallum@gmail.com>
2022-01-31 20:39:44 -0500

    Create boilerplate with create-react-app

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10" type="email" placeholder="jane@example.com" />
+    </div>
+  );
+}
+
+export default App;
