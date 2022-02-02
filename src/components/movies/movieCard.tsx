d668acf61548d787d584db6c7bffa970d3876f10
Callum Gundlach <gundlachcallum@gmail.com>
2022-02-02 16:37:32 -0500

    Integrate thumbs up or down buttons

--- src/components/movies/movieCard.tsx
+++ src/components/movies/movieCard.tsx
@@ -1,57 +1,50 @@
 import React, {useEffect, useState} from 'react';
-import {motion, useMotionValue, useDragControls} from 'framer-motion';
+import {motion, useMotionValue, useDragControls, AnimatePresence, useTransform} from 'framer-motion';
 import {useWindowWidth} from '@react-hook/window-size';
 
 import MovieInfoCard from './movieInfoCard';
 
+// Swiping threshold
+const threshold = 0.4;
+const colorThreshold = 0.1;
+
+// Main component
 interface IProps {
-  name: string;
   onChange: (liked: boolean) => void;
+  liked?: boolean;
   active?: boolean;
-  idx: number;
 }
 
 const MovieCard: React.FC<IProps> = props => {
-  const {onChange, active, idx, name} = props;
+  const {onChange, active, liked} = props;
 
   const dragControls = useDragControls();
   const x = useMotionValue(0);
 
   const width = useWindowWidth();
 
-  const [rotate, setRotate] = useState<number>(0);
-  const [liked, setLiked] = useState<boolean | undefined>();
-
-  // Trigger event handler
-  useEffect(() => {
-    if (liked !== undefined) onChange(liked);
-  }, [liked]);
-
-  // Update rotation
-  useEffect(() => {
-    const unsubscribeX = x.onChange(pointX => {
-      setRotate(Math.trunc((pointX / width) * 20));
-    });
-
-    return () => unsubscribeX();
-  });
+  const greenOpacity = useTransform(x, [width / 10, width], [0, 1]);
+  const redOpacity = useTransform(x, [-width, -width / 10], [1, 0]);
 
   // Update variants
+  let exitX = '0';
+  if (liked !== undefined) exitX = liked ? '100vw' : '-100vw';
+
   const variants = {
-    initial: {y: -25, x: 0, opacity: 0},
-    enter: {y: 0, x: 0, rotate, opacity: 1, transition: {duration: 0.25}},
-    exit: {y: -25, x: liked ? '100vw' : '-100vw', opacity: 0, transition: {duration: 0.25}},
+    initial: {y: 10, x: 0, scale: 0.95, opacity: 0},
+    enter: {y: 0, x: 0, scale: 1, opacity: 1, transition: {duration: 0.25}},
+    exit: {x: exitX, opacity: 1, transition: {duration: 0.25}},
   };
 
   return (
-    <div className="absolute inset-0" style={{zIndex: idx}}>
+    <div className="absolute inset-0">
       <motion.div
         variants={variants}
+        style={{x}}
         initial="initial"
         animate="enter"
         exit="exit"
         drag={active ? 'x' : false}
-        style={{x}}
         dragControls={dragControls}
         dragListener={false}
         dragConstraints={{left: 0, right: 0}}
@@ -59,10 +52,10 @@ const MovieCard: React.FC<IProps> = props => {
         whileDrag={{scale: 1.01}}
         onDragEnd={(e, t) => {
           const relativeOffset = (t.point.x - width / 2) / width;
-          if (relativeOffset > 0.4) {
-            setLiked(true);
-          } else if (relativeOffset < -0.4) {
-            setLiked(false);
+          if (relativeOffset > threshold) {
+            onChange(true);
+          } else if (relativeOffset < -threshold) {
+            onChange(false);
           }
         }}
       >
@@ -72,8 +65,10 @@ const MovieCard: React.FC<IProps> = props => {
           }`}
         >
           <div className="inset-0 absolute z-10" onPointerDown={e => active && dragControls.start(e)}>
+            <motion.div className="absolute bottom-0 inset-0 z-10 bg-red-500" style={{opacity: redOpacity}} />
+            <motion.div className="absolute bottom-0 inset-0 z-10 bg-green-500" style={{opacity: greenOpacity}} />
             <img className="object-cover h-full w-full" src="/movies/blade-runner.jpg" />
-            <div className="absolute bottom-0 inset-x-0 h-24 from-black bg-gradient-to-t" />
+            <div className="absolute bottom-0 inset-x-0 z-0 h-24 from-black bg-gradient-to-t" />
           </div>
           <MovieInfoCard />
         </div>
--- src/pages/swipe-party/SwipeParty.tsx
+++ src/pages/swipe-party/SwipeParty.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {AnimatePresence} from 'framer-motion';
 
 import Container from '../../components/layout/container';
 import MovieCard from '../../components/movies/movieCard';
@@ -14,33 +15,36 @@ const SwipeParty: React.FC = () => {
   const [mode, setMode] = useState<swipeMode>('swiping');
 
   const [stack, setStack] = useState<string[]>([]);
+  const [liked, setLiked] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
     setStack(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']);
   }, []);
 
-  const card = (
-    <MovieCard
-      key={stack[stack.length - 1]}
-      onChange={liked => {
-        setStack(pop(stack));
-      }}
-      name={stack[stack.length - 1]}
-      idx={1}
-      item-value={stack[stack.length - 1]}
-      active
-    />
-  );
+  useEffect(() => {
+    if (liked !== undefined) setStack(pop(stack));
+  }, [liked]);
+  useEffect(() => {
+    setLiked(undefined);
+  }, [stack]);
+
+  const onChange = (like: boolean) => setLiked(like);
+
+  const curElement = stack[stack.length - 1];
 
   return (
     <Container>
       <div className="flex flex-col space-y-8 w-full h-full">
-        <div className="relative h-[70vh]">{card}</div>
+        <div className="relative h-[70vh]">
+          <AnimatePresence exitBeforeEnter>
+            <MovieCard key={curElement} liked={liked} onChange={onChange} active />
+          </AnimatePresence>
+        </div>
 
         <div className="flex flex-row justify-center">
           <div className="flex flex-row space-x-16">
-            <ThumbButton />
-            <ThumbButton variant="like" />
+            <ThumbButton onClick={() => onChange(false)} />
+            <ThumbButton onClick={() => onChange(true)} variant="like" />
           </div>
         </div>
 
