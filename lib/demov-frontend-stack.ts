1f7f011b217a9b44e3d4882b50006d5b7d334dc0
Callum Gundlach <gundlachcallum@gmail.com>
2022-02-07 01:35:41 -0500

    Make output env specific

--- lib/demov-frontend-stack.ts
+++ lib/demov-frontend-stack.ts
@@ -44,7 +44,7 @@ export class DemovFrontendStack extends cdk.Stack {
       },
     });
 
-    new cdk.CfnOutput(this, 'cloudfront-url', {
+    new cdk.CfnOutput(this, envSpecific('cloudfront-url'), {
       value: dist.domainName,
       description: 'The URL of the cloudfront distribution.',
       exportName: 'cloudfront-url',
